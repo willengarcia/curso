4ff9dd0becb9050276d5c1f6eec35a8c6ee2b4d0
Will <willen.garcia05@gmail.com>
2024-04-02 15:04:41 -0300

    Segunda parte de JS

--- aula010/exe_javascript/basico/javascript.js
+++ aula010/exe_javascript/basico/javascript.js
@@ -84,18 +84,107 @@ if (r >= 6) {
     Negação: !
 */
 
+/*
 var nota01 = prompt('Digite sua nota: ')
 var nota02 = prompt('Digite sua segunda nota: ')
-parseInt.nota01
-parseInt.nota02
-var r = (nota01+nota02)
-parseFloat.r
+var n1 = parseFloat(nota01)
+var n2 = parseFloat(nota02)
+var r = (n1+n2)/2
 console.log(r, nota01, nota02)
 if(r >=10 || r <=7){
-    document.write('<h3>Parabéns, você passou, com a média de: '+r+'</h3>')
+    document.write('<h3>Parabéns, você passou, com a média de: '+r.toFixed(2)+'</h3>')
 } else if(r <= 6 || r >=5) {
-    document.write('<h3>Você está de recuperação, com a média de: '+r+'</h3>')
+    document.write('<h3>Você está de recuperação, com a média de: '+r.toFixed(2)+'</h3>')
 } else {
-    document.write('<h3>Você foi reprovado, com a média de: '+r+'</h3>')
+    document.write('<h3>Você foi reprovado, com a média de: '+r.toFixed(2)+'</h3>')
+}
+*/
 
+/* Operador Ternário 
+    var teste = (condicao) ? 'verdadeiro' : 'falso'
+
+var n1 = prompt('Digite um número: ')
+var nu = parseInt(n1)
+var teste = 10 >= nu && 10 <= nu ? document.write('<h1> A condição foi verdadeira!</h1>') : alert('A condição é falsa')
+*/
+
+/* DESAFIO IDADE
+var entrada01 = prompt('Digite sua idade: ')
+var idade = parseInt(entrada01)
+
+if ((idade >= 0)  && (idade < 15)){
+    console.log('Criança')
+} else if ((idade >= 15) && (idade < 30)){
+    console.log('Jovem')
+} else if ((idade >= 30) && (idade < 60)){
+    console.log('Adulto')
+} else {
+    console.log('Idoso')
+}
+*/
+
+/* SWITCH (Caso se), muito usado em menu
+    var opcao = 2
+    switch(opcao){
+        case 1:
+            (instrução se a opção for 1)
+            break
+        case 2:
+            (instrução se a opção for 2)
+            break
+        default:
+            (instrução se não for nenhuma das opções)
+            break
+    }
+*/
+var opcao = prompt('Digite uma opção: ')
+switch (opcao) {
+    case 'soma':
+        document.write('<h1>Você escolheu a opção: Soma.</h1>')
+        var n1 = prompt('Digite o número: ')
+        var n2 = prompt('Digite outro número: ')
+        var num1 = parseInt(n1)
+        var num2 = parseInt(n2)
+        var res = num1 + num2
+        document.write('<h2>Resultado de: '+num1+'+'+num2+'= '+res+'</h2>')
+        break;
+    case 'subtrair':
+        document.write('<h1>Você escolheu a opção: Soma.</h1>')
+        var n1 = prompt('Digite o número: ')
+        var n2 = prompt('Digite outro número: ')
+        var num1 = parseInt(n1)
+        var num2 = parseInt(n2)
+        var res = num1 - num2
+        document.write('<h2>Resultado de: '+num1+'+'+num2+'= '+res+'</h2>')
+        break;
+    case 'dividir':
+        document.write('<h1>Você escolheu a opção: Soma.</h1>')
+        var n1 = prompt('Digite o número: ')
+        var n2 = prompt('Digite outro número: ')
+        var num1 = parseInt(n1)
+        var num2 = parseInt(n2)
+        var res = num1 / num2
+        document.write('<h2>Resultado de: '+num1+'+'+num2+'= '+res+'</h2>')
+        break;
+    case 'multiplicar':
+        document.write('<h1>Você escolheu a opção: Soma.</h1>')
+        var n1 = prompt('Digite o número: ')
+        var n2 = prompt('Digite outro número: ')
+        var num1 = parseInt(n1)
+        var num2 = parseInt(n2)
+        var res = num1 * num2
+        document.write('<h2>Resultado de: '+num1+'+'+num2+'= '+res+'</h2>')
+        break;
+    case 'modulo':
+        document.write('<h1>Você escolheu a opção: Soma.</h1>')
+        var n1 = prompt('Digite o número: ')
+        var n2 = prompt('Digite outro número: ')
+        var num1 = parseInt(n1)
+        var num2 = parseInt(n2)
+        var res = num1 % num2
+        document.write('<h2>Resultado de: '+num1+'+'+num2+'= '+res+'</h2>')
+        break;
+    default:
+        document.write('<p>Você escolheu uma opção inválida!</p>')
+        break;
 }
