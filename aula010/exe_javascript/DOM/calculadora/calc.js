0788bb9454eeb6acaaa711a6367221f14dce797e
Will <willen.garcia05@gmail.com>
2024-04-09 10:34:13 -0300

    Fazendo calculadora com js e bootstrap

--- aula010/exe_javascript/DOM/calculadora/calc.js
+++ aula010/exe_javascript/DOM/calculadora/calc.js
@@ -1,4 +1,18 @@
-function operacao(){
-    var op = document.getElementById('adicao').value
-    console.log(op)
+function calcular(tipo, valor){
+    if (tipo === 'acao'){
+        if(valor === 'c'){
+            document.getElementById('resultado').value = ''
+        }
+        if(valor == '+' || valor == '-' || valor == '/' || valor == '*' || valor == '.'){
+            document.getElementById('resultado').value +=valor
+        }
+        if(valor === '='){
+            var valor_campo = document.getElementById('resultado').value 
+            document.getElementById('resultado').value = eval(valor_campo) // função eval, pega a string e 'converte' em um número, o qual pode ser efetuado a operação
+
+        }
+    } else if (tipo == 'valor'){
+        document.getElementById('resultado').value += valor
+
+    }
 }
