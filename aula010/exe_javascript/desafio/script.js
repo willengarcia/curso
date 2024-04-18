179ce91bb6155aca958fe6c959f05bc728d2a80f
Will <willen.garcia05@gmail.com>
2024-04-18 12:09:40 -0300

    Finalização parte 1 do jogo mosquito

--- aula010/exe_javascript/desafio/script.js
+++ aula010/exe_javascript/desafio/script.js
@@ -1,30 +1,55 @@
 function start(){
-    window.location.replace('http://127.0.0.1:5500/aula010/exe_javascript/desafio/index.html')
+    window.location.replace('http://127.0.0.1:5500/aula010/exe_javascript/desafio/start.html')
     var dificuldade = document.getElementById('nivel').value
     console.log(dificuldade)
-}
+}    
+var vida = 1
 function aparecerMosca(){
+
+    // Removendo o mosquito anterior, caso exista
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+        document.getElementById('v'+vida).src = 'imagens/coracao_vazio.png'
+        vida++
+        if (vida > 3){
+            window.location.replace('http://127.0.0.1:5500/aula010/exe_javascript/desafio/game_over.html')
+        }
+        
+    }
+    // Encapsulando a altura e largura da página
     var altura = window.innerHeight
     var largura = window.innerWidth
     var posicaoX = Math.floor(Math.random() * largura)- 90
     var posicaoY = Math.floor(Math.random() * altura)- 90
     posicaoX = posicaoX <0 ? 0 : posicaoX
     posicaoY = posicaoY <0 ? 0 : posicaoY
+
     // Criando elemento img
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosca.png'
-    mosquito.className = 'mosca'
+    mosquito.className = tamanho()
     mosquito.style.left = posicaoX +'px'
     mosquito.style.top = posicaoY +'px'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove() // faz referência ao próprio elemento que está a função
+    }
     document.body.appendChild(mosquito)
+    console.log(tamanho())
 }
-function muda(p){
-    i = 99
-    var x = setInterval(function(){
-        document.getElementById('time').innerText = i
-        i--
-        if (i<0){
-            clearInterval(x)
-        } 
-    }, 1000)
+function tamanho(){
+    var classe = Math.floor(Math.random()*3)
+    switch(classe){
+        case 0:
+            return 'mosca1'
+        case 2:
+            return 'mosca2'
+        case 3:
+            return 'mosca3'
+        default:
+            return 'mosca2'
+    }
+}
+function tiraVida(){
+
 }
