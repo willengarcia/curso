45bc7d5af14769dd9b0fac7951f851e21427d821
Will <willen.garcia05@gmail.com>
2024-05-22 17:27:14 -0300

    sla

---
+++ aula011/desafio_Orcamento/script.js
@@ -0,0 +1,27 @@
+const botao = document.querySelector('.botoes')
+const consulta = document.querySelector('#consulta')
+const cadastro = document.querySelector('#cadastro')
+const informacao = document.querySelector('.infor')
+const tabela = document.getElementById('#tabela')
+
+botao.addEventListener('click', (evento)=>{
+    const data = document.querySelector('#data').value
+    const selecao = document.querySelector('#selecao').value
+    const descricao = document.querySelector('#descricao').value
+    const valor = document.querySelector('#valor').value
+    console.log(data)
+    console.log(selecao)
+    console.log(descricao)
+    console.log(valor)
+})
+consulta.addEventListener('click', (evento)=>{
+    const text = document.querySelector('h1')
+    const tabela = document.getElementById('tabela')
+    text.textContent = 'Consulta de despesas'
+    tabela.style.display = 'block'
+    tabela.style.width = '100%'
+})
+cadastro.addEventListener('click', (evento)=>{
+    const text = document.querySelector('h1')
+    text.textContent = 'Registro de nova despesa'
+})
