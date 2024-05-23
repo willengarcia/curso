1665c48dfb6bd83a470b19300bd396dc98def049
Will <willen.garcia05@gmail.com>
2024-05-23 11:13:27 -0300

    Finalização parte I do desafio Orcamento

--- aula011/desafio_Orcamento/script.js
+++ aula011/desafio_Orcamento/script.js
@@ -4,25 +4,44 @@ const cadastro = document.querySelector('#cadastro')
 const informacao = document.querySelector('.infor')
 const tabela = document.getElementById('#tabela')
 
+const criaLinhaTabela = ()=>{
+    const tabela = document.querySelector('tbody')
+    const linha = document.createElement('tr')
+    tabela.appendChild(linha)
+
+    for(let l =1 ; l<=4; l++){
+        let dado = document.createElement('td')
+        linha.appendChild(dado)
+    }
+    return linha
+}
+
 botao.addEventListener('click', (evento)=>{
     const data = document.querySelector('#data').value
     const selecao = document.querySelector('#selecao').value
     const descricao = document.querySelector('#descricao').value
     const valor = document.querySelector('#valor').value
-    console.log(data)
-    console.log(selecao)
-    console.log(descricao)
-    console.log(valor)
+    if((data == '') || (selecao == '') || (descricao == '') || (valor == '')){
+        alert('Tem que preencher todos os dados!')
+    }else{
+        const linhaCriada = criaLinhaTabela()
+        const dadosFilho = [...linhaCriada.children]
+        dadosFilho[0].textContent = data
+        dadosFilho[1].textContent = selecao
+        dadosFilho[2].textContent = descricao
+        dadosFilho[3].textContent = valor
+    }
+    
 })
 consulta.addEventListener('click', (evento)=>{
     const text = document.querySelector('h1')
     const tabela = document.getElementById('tabela')
     text.textContent = 'Consulta de despesas'
-    tabela.style.opacity = '1'
+    tabela.style.visibility = 'visible'
 })
 cadastro.addEventListener('click', (evento)=>{
     const text = document.querySelector('h1')
     const tabela = document.getElementById('tabela')
-    tabela.style.opacity = '0'
+    tabela.style.visibility = 'hidden'
     text.textContent = 'Registro de nova despesa'
 })
