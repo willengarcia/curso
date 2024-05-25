dd004d6d8a6599ae714191046109ca0415a7c6bb
Will <willen.garcia05@gmail.com>
2024-05-25 11:29:21 -0300

    Reduzindo código

--- aula011/desafio_Orcamento/script.js
+++ aula011/desafio_Orcamento/script.js
@@ -3,12 +3,10 @@ const consulta = document.querySelector('#consulta')
 const cadastro = document.querySelector('#cadastro')
 const informacao = document.querySelector('.infor')
 const tabela = document.getElementById('#tabela')
-
 const criaLinhaTabela = ()=>{
     const tabela = document.querySelector('tbody')
     const linha = document.createElement('tr')
     tabela.appendChild(linha)
-
     for(let l =1 ; l<=4; l++){
         let dado = document.createElement('td')
         dado.setAttribute('class', 'desc')
@@ -36,12 +34,7 @@ class Bd{
         return parseInt(proximoId)+1
     }
     gravar(d){
-        // identificação do objeto, dado (precisa ser encmainhado através da notaçao JSON)
-    
-        // notacao JSON >'{"chave":"valor"}'
-        // obj Literal > {chave:"valor"}
-        // Usar a lib JSON com o metodo stringfy(objeto literal) - conversão em JSON
-        // Usar a lib JSON com o metodo parse(objeto JSON) - para conversão em obj Literal
+        // identificação do objeto, dado (precisa ser encmainhado através da notaçao JSON). Notacao JSON >'{"chave":"valor"}'. Obj Literal > {chave:"valor"}. Usar a lib JSON com o metodo stringfy(objeto literal) - conversão em JSON. Usar a lib JSON com o metodo parse(objeto JSON) - para conversão em obj Literal
         let id = this.getProximoId()
         localStorage.setItem(id, JSON.stringify(d))
         localStorage.setItem('id', id) 
@@ -55,62 +48,51 @@ class Bd{
         }
     }
 }
-const alerta = ()=>{
+const alerta = (mensagem, cor)=>{
     let div = document.querySelector('.container')
     let h2 = document.createElement('h2')
     let sapn = document.createElement('span')
     div.firstElementChild.appendChild(h2)
     h2.appendChild(sapn)
-    return h2
+
+    let tempo = 5
+    let cronometro = setInterval(function(){
+        tempo--
+        document.querySelector('h2').textContent = mensagem
+        document.querySelector('h2').style.color = cor
+    }, 1000)
+    setTimeout(function(){
+        clearInterval(cronometro)
+        h2.remove()
+    }, 2000)
 }
 // localStorage: Persiste no navegador
 function cadastrarDespesa(){
-    let data = document.getElementById('data')
-    let selecao = document.getElementById('selecao')
-    let descricao = document.getElementById('descricao')
-    let valor = document.getElementById('valor')
-    if((data.value == '') || (selecao.value == '') || (descricao.value == '') || (valor.value == '')){
-        let tempo = 5
-        const a = alerta()
-        let cronometro = setInterval(function(){
-            tempo--
-            document.querySelector('h2').textContent = 'Preencha todos os dados!'
-            document.querySelector('span').textContent = tempo
-        }, 1000)
-        setTimeout(function(){
-            clearInterval(cronometro)
-            a.remove()
-        }, 2000)
+    let data = document.getElementById('data').value
+    let selecao = document.getElementById('selecao').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+    if((data == '') || (selecao == '') || (descricao == '') || (valor == '')){
+        const a = alerta('Insira todos os dados!', 'red')
     }else{
         const linhaCriada = criaLinhaTabela()
         const dadosFilho = [...linhaCriada.children]
-        dadosFilho[0].textContent = data.value
-        dadosFilho[1].textContent = selecao.value
-        dadosFilho[2].textContent = descricao.value
-        dadosFilho[3].textContent = valor.value
+        dadosFilho[0].textContent = data
+        dadosFilho[1].textContent = selecao
+        dadosFilho[2].textContent = descricao
+        dadosFilho[3].textContent = valor
         let despesas = new Despesa(
-            data.value,
-            selecao.value,
-            descricao.value,
-            valor.value
+            dadosFilho[0].textContent,
+            dadosFilho[1].textContent,
+            dadosFilho[2].textContent,
+            dadosFilho[3].textContent,
         )
         let bd = new Bd()
         bd.gravar(despesas)
-        let tempo = 2
-        const a = alerta()
-        let cronometro = setInterval(function(){
-            document.querySelector('h2').textContent = 'Dados inseridos com sucesso'
-            document.querySelector('h2').style.color = 'blue'
-            tempo--
-        }, 1000) // Cronometro que remove o h2
-        setTimeout(function(){
-            clearInterval(cronometro)
-            a.remove()
-        }, 2000) // Cronometro de 5s
+        const a = alerta('Dados inseridos com sucesso!', 'blue')
     }
 }
 let bd = new Bd()
-
 botao.addEventListener('click', (evento)=>{
     if(botao.textContent =='Adicionar'){
         cadastrarDespesa()
@@ -124,10 +106,8 @@ consulta.addEventListener('click', (evento)=>{
     text.textContent = 'Consulta de despesas'
     tabela.style.visibility = 'visible'
     botao.textContent = 'Pesquisar'
-
     consulta.style.backgroundColor = 'white'
     consulta.style.color = 'blue'
-
     cadastro.style.backgroundColor = 'rgba(55, 68, 248, 0.911)'
     cadastro.style.color = 'white'
 })
@@ -137,10 +117,8 @@ cadastro.addEventListener('click', (evento)=>{
     tabela.style.visibility = 'hidden'
     text.textContent = 'Registro de nova despesa'
     botao.textContent = 'Adicionar'
-
     cadastro.style.backgroundColor = 'white'
     cadastro.style.color = 'blue'
-
     consulta.style.backgroundColor = 'rgba(55, 68, 248, 0.911)'
     consulta.style.color = 'white'
 })
